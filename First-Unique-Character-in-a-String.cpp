58a4099882d79604503758935f56212d3bdf8bc9
ALOK PAUL <91766242+ProgrammerAlok@users.noreply.github.com>
2023-06-25 17:13:37 +0530

    Create First-Unique-Character-in-a-String.cpp

---
+++ First-Unique-Character-in-a-String.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int helper(string &s)
+{
+    // {char, {count, index}}
+    map< char, pair<int, int> > m;
+    queue<char> que;
+    for(int i=0; i<s.size(); ++i){
+        if(m.find(s[i])!=m.end()){
+            m[s[i]].first++;
+        }
+        else{
+            m.insert({s[i], {1, i}});
+            que.push(s[i]);
+        }
+    }
+    while(!que.empty()){
+        auto it = *m.find(que.front());
+        que.pop();
+        if(it.second.first == 1) return it.second.second;
+    }
+    return -1;
+}
+
+int main()
+{
+  vector<int> a = "input";
+  cout << helper(a);
+
+  return 0;
+}
+
